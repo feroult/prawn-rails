f085d66612f36ea5ee92c96bd5730bed74033228
Fernando Ultremare <fernando@6spot.com.br>
2011-08-30 22:11:33 -0300

    some format utils

--- lib/prawn_rails.rb
+++ lib/prawn_rails.rb
@@ -3,6 +3,7 @@ require 'prawn'
 require 'prawn_rails/template_handler'
 require 'prawn_rails/helper'
 require 'prawn_rails/wrapper'
+require 'prawn_rails/highlight_callback'
 
 #if Prawn::VERSION == "0.8.4"
 #  require 'prawn/layout'
--- lib/prawn_rails/wrapper.rb
+++ lib/prawn_rails/wrapper.rb
@@ -7,11 +7,18 @@ module Prawn
         self.pdf = pdf      
       end
       
-      def section(options, &block)
+      def box(options = {}, &block)
         left = ( options.delete(:left) or 0 )
         top = ( options.delete(:top) or cursor )        
         options.merge!(:width => bounds.right) unless options[:width]
         bounding_box([left, top], options, &block)
+      end
+      
+      def section(options = {}, &block)
+        stroke_horizontal_rule
+        move_down 3       
+        box(options, &block)
+        move_down 5 
       end
       
       def line(&block)      
@@ -20,12 +27,35 @@ module Prawn
         text "\n"
       end
       
-      def field(s, options)      
-        float do
-          section(options) do
-            text s
+      def field(s, options = {})      
+        float do        
+          label = options.delete(:label)
+          align = ( options.delete(:align) or :left )
+          highlight = ( options.delete(:highlight) or false )         
+          
+          box(options) do          
+            highlight_text(text_with_label(s, label)) if highlight 
+            text formatted_text_with_label(s, label), :inline_format => true, :align => align
           end
         end
+      end
+
+      def highlight_text(s)      
+        width = width_of(s)
+        height = height_of(s)
+      
+        original_color = fill_color
+        pdf.fill_color = "ffff00"
+        pdf.fill_rectangle([bounds.left-2, bounds.top+2], width+4, height)
+        pdf.fill_color = original_color             
+      end
+
+      def text_with_label(s, label)       
+        label.nil? ? s : "#{label}: #{s}"
+      end
+
+      def formatted_text_with_label(s, label)          
+        label.nil? ? s : "<b>#{label}:</b> #{s}"
       end
       
       def method_missing(method, *args, &block) 
